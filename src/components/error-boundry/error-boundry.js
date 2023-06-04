ecf31d4fd5699d3da1505935545b83b852f2cb1a
motorolla29 <eutyou@gmail.com>
2023-06-04 18:40:55 +0300

    basic react+redux application structure

--- src/components/error-boundry/error-boundry.js
+++ src/components/error-boundry/error-boundry.js
@@ -1,9 +1,18 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import Error from '../error';
 
 export default class ErrorBoundry extends Component {
-    render() {
-       
-        return 1;
-    }
+  state = {
+    errror: false,
+  };
+
+  componentDidCatch() {
+    this.setState({ errror: true });
+  }
+
+  render() {
+    if (this.state.error) return <Error />;
+
+    return this.props.children;
+  }
 }
